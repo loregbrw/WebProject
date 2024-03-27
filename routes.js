e8391111f242fb3b99397d86e5fe055e0c2056b2
loregbrw <loregobara@gmail.com>
2024-03-27 15:33:50 -0300

    Added new index page

---
+++ routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const route = express.Router();
+// Importando os Controllers
+const home = require('./src/controllers/home');
+// Iniciando as rotas
+route.get('/', home.pagInicialGet);
+module.exports = route;
