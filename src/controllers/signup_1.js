8cf44d1e7629c7287f2772735e1983562ce754a0
loregbrw <loregobara@gmail.com>
2024-04-02 16:48:16 -0300

    Starting database

--- src/controllers/signup_1.js
+++ src/controllers/signup_1.js
@@ -1,5 +1,17 @@
+const user = require('../model/user_login');
+
 module.exports = {
     async pagSignup1Get(req, res) {
         res.render('../views/signup_1');
+    },
+
+    async pagSignup1Post(req, res) {
+        const data = req.body;
+
+        await user.create ({
+            email: data.user_email,
+            username: data.user_username,
+            password: data.user_password,
+        });
     }
 }
