87ffeacf2a415c233bbb4fb5df1a3dd63ed182db
Mariasina <carolbeveer@gmail.com>
2024-04-03 14:50:55 -0300

    started working on select color js

---
+++ public/js/select_color.js
@@ -0,0 +1,25 @@
+
+// const color_selector = document.getElementById("color-box");
+// const color = document.querySelector('input[type=color]');
+
+// const { text } = require("express");
+
+// color_selector.style.backgroundColor = color.value;
+    
+const background_input = document.getElementById('color-selector-background');
+const font_input = document.getElementById('color-selector-font');
+const text_box = document.getElementById('color-box');
+
+background_input.addEventListener('input', function() {
+    const selected_color = this.value;
+
+    text_box.style.backgroundColor = selected_color;
+    console.log(selected_color);
+});
+
+font_input.addEventListener('input', function() {
+    const selected_color = this.value;
+
+    text_box.style.color = selected_color;
+    console.log(selected_color);
+});
