797cef97c91387dec14d32fffd35fded5cd353c8
Mariasina <carolbeveer@gmail.com>
2024-04-08 14:26:18 -0300

    finished responsiveness from edit recipes, add recipes and view recipes pages

--- src/controllers/view_recipes.js
+++ src/controllers/view_recipes.js
@@ -1,14 +1,14 @@
 module.exports = {
     async pagViewRecipesGet(req, res) {
-        const parametro = req.params.recipe;
+        // const parametro = req.params.recipe;
 
-        const this_recipe = await user.findOne({ 
-            where: {
-                id_recipe: parametro
-            },
-            attributes: ['id_recipe', 'duration', 'portions', 'status', 'description', 'image']
-        });
+        // const this_recipe = await user.findOne({ 
+        //     where: {
+        //         id_recipe: parametro
+        //     },
+        //     attributes: ['id_recipe', 'duration', 'portions', 'status', 'description', 'image']
+        // });
 
-        res.render('../views/view_recipes', {this_recipe});
+        res.render('../views/view_recipes');
     }
 }
