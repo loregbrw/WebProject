89d25d8b1e5346451c4e6f762fbc2bebae777b5b
loregbrw <loregobara@gmail.com>
2024-04-09 11:20:24 -0300

    Started the calendar backend

--- src/controllers/calendar.js
+++ src/controllers/calendar.js
@@ -1,3 +1,27 @@
+function today() {
+    const today = new Date();
+
+    return today;
+}
+
+function thisMonth() {
+    const today = new Date();
+    const month = today.getMonth();
+
+    return month;
+}
+
+function thisYear() {
+    const today = new Date();
+    const year = today.getFullYear() - birth.getFullYear();
+
+    if (month < 0 || (month === 0 && today.getDate() < birth.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
 module.exports = {
     async pagCalendarGet(req, res) {
         res.render('../views/calendar');
