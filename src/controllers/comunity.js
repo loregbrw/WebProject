4f5c8ac313c3dccf5a91885f13e5de7b34ce5031
Mariasina <carolbeveer@gmail.com>
2024-04-09 15:50:48 -0300

    working on dark mode on all pages

--- src/controllers/comunity.js
+++ src/controllers/comunity.js
@@ -1,5 +1,17 @@
+const user = require('../model/Users');
+
 module.exports = {
     async pagComunityGet(req, res) {
-        res.render('../views/comunity');
+        const parametro = req.params.username;
+
+        const this_user = await user.findOne({
+            where: {
+                username: parametro
+            },
+            attributes: ['id_user', 'name', 'email', 'password', 'birthdate', 'username', 'image', 'description']
+
+        });
+
+        res.render('../views/comunity', {this_user});
     }
 }
