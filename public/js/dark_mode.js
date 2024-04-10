d20e0a79d736c02bc061013b9af70f2bbf7dd159
Mariasina <carolbeveer@gmail.com>
2024-04-10 13:32:38 -0300

    dark mode working when you change pages

--- public/js/dark_mode.js
+++ public/js/dark_mode.js
@@ -1,21 +1,27 @@
 const dark_mode = document.getElementById("dark_mode_icon");
 const html = document.querySelector("html");
 
-let dark_mode_on = 0; 
+let dark_mode_on = localStorage.getItem("dark_mode_item");
+if (dark_mode_on === null) {
 
-dark_mode.addEventListener("click", function(){
-    html.classList.toggle("dark-mode");
+    dark_mode_on = "0";
+}
 
-    if(dark_mode_on == 0){
-        dark_mode_on = 1;
-    }
-    else{
-        dark_mode_on = 0;
-    }
+if (dark_mode_on === "1") {
+    html.classList.add("dark-mode");
+}
 
-    dark_mode = this.setAttribute('src', `/img/icon-dark-mode-${dark_mode_on}.png`);
+dark_mode.addEventListener("click", function () {
+    dark_mode.setAttribute('src', `/img/icon-dark-mode-${dark_mode_on}.png`);
+    if (dark_mode_on === "0") {
     
-    console.log("click");
+        dark_mode_on = "1";
+        localStorage.setItem("dark_mode_item", "1");
+        html.classList.add("dark-mode");
+    } 
+    else {
+        dark_mode_on = "0";
+        localStorage.setItem("dark_mode_item", "0");
+        html.classList.remove("dark-mode");
+    }
 });
-
-
