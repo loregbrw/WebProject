95a62d75068cf286d645361ead0b12e59743f59b
Mari <carolbeveer@gmail.com>
2024-04-11 11:03:06 -0300

    working on adding recipes to calendar

--- src/controllers/calendar.js
+++ src/controllers/calendar.js
@@ -1,4 +1,7 @@
 const user = require('../model/Users');
+const user_calendar = require('../model/User_calendar');
+const calendar = require('../model/Calendar');
+
 
 function getToday() {
     return new Date();
@@ -20,9 +23,14 @@ function getThisYear(date) {
     return date.getFullYear();
 }
 
+function getDateDay(date){
+    return date.getDate();
+}
+
 module.exports = {
     async pagCalendarGet(req, res) {
         const parametro = req.params.username;
+        const parametro_day = req.params.id_calendar;
         
         const this_user = await user.findOne({
             where: {
@@ -31,12 +39,29 @@ module.exports = {
             attributes: ['id_user', 'name', 'email', 'password', 'birthdate', 'username', 'image', 'description']
         });
 
+        const this_calendar = await calendar.findAll({
+            where: {
+                day: parametro_day
+            }
+        });
+
+        const this_user_calendar = await user_calendar.findOne({
+            where: {
+                calendar_id: this_calendar.id_calendar
+            }
+        });
+
+
         const today = getToday();
         console.log(today);
         const month_num = getThisMonth(today);
         const month_name = getMonthName(month_num);
         const year = getThisYear(today);
 
-        res.render('../views/calendar', { this_user, today, month_num, month_name, year });
+        const this_day = getDateDay(this_user_calendar);
+        const this_month = getThisMonth(this_user_calendar);
+        const this_year = getThisYear(this_user_calendar);
+
+        res.render('../views/calendar', { this_user, today, month_num, month_name, year, this_day, this_month, this_year });
     }
 }
